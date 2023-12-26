1ec885ca1ffc60475fe990ab0a13b61fc2102556
CSylvie <sylvie.castet@gmail.com>
2023-12-26 14:09:32 +0100

    ajoute fichiers sass de base

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import logo from '../assets/logo.svg';
-// import './styles/_header.scss';
+import '../styles/Header.css';
 
 function Header() {
   return (
     <div className="Header">
         <img src={logo} className="Header-logo" alt="logo" />
+        <p>TEST POUR VOIR</p>
     </div>
   );
 }
