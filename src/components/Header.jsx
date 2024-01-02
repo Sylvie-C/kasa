b7d156380e026a7ffceaf1235c41b93dd97e8139
CSylvie <sylvie.castet@gmail.com>
2024-01-02 16:55:09 +0100

    corrige navbar header , ajoute style liens navbar (souligné si actif)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,15 @@
 import logo from "../assets/images/logo.svg";
 
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <img src={logo} className="header__logo" alt="logo" />
-
-      <NavLink className="header__navlink">
-        <Link to="/" className="header__link">
-          Accueil
-        </Link>
-        <Link to="/apropos" className="header__link">
-          À propos
-        </Link>
-      </NavLink>
+      <div>
+        <NavLink to="/" className="header__link">Accueil</NavLink>
+        <NavLink to="/apropos" className="header__link">À propos</NavLink>
+      </div>
     </div>
   );
 }
