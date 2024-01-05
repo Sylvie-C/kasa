2dc73a53fa34b59b1f8bf8850b8131046f07660c
CSylvie <sylvie.castet@gmail.com>
2024-01-05 08:47:49 +0100

    reorganise structure projet (supprime fichiers inutiles)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import Hero from "../components/Hero" ; 
-import GalleryCards from "../components/GalleryCards";
+import galleryResource from "../data/logements.json" ; 
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="homepage pageContent">
             <Hero/> 
-            <GalleryCards/>
+            <section className="gallery">
+                { 
+                    galleryResource.map ( (elt) => (	
+                        
+                        <Link to="/accomodation/" className="gallery__card" key={elt.id}> 
+                            <img src={elt.cover} alt="appartement"></img>
+                            <p>{elt.title}</p>
+                        </Link> 
+                        ) 
+                    ) 
+                }
+            </section>
         </div>
     )
 }
