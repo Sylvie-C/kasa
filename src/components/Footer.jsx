632802dbcb8ef88f27f2687ee9b0d595e7327f1a
CSylvie <sylvie.castet@gmail.com>
2024-01-19 15:43:10 +0100

    corrige style footer et page accomodation (responsive)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import logo from "../assets/images/logo-white.svg";
 function Footer() {
   return (
     <div className="footer">
-      <img src={logo} className="footer__logo" alt="logo" />
-      &#x00A9; 2020 Kasa. All rights reserved.
+      <div className="footer__container">
+        <img src={logo} className="footer__logo" alt="logo" />
+        &#x00A9; 2020 Kasa. All rights reserved.
+      </div>
     </div>
   );
 }
