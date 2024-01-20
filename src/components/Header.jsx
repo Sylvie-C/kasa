463e9a3fc12a88cc82f9334e39c1c4ca756327bb
CSylvie <sylvie.castet@gmail.com>
2024-01-20 19:56:01 +0100

    ajoute lien vers page accueil sur logo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from "../assets/images/logo.svg";
 
-import { NavLink } from "react-router-dom";
+import { Link , NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img src={logo} className="header__logo" alt="logo" />
+      <Link to="/"><img src={logo} className="header__logo" alt="logo" /></Link>
       <div>
         <NavLink to="/" className="header__link">Accueil</NavLink>
         <NavLink to="/apropos" className="header__link">A propos</NavLink>
